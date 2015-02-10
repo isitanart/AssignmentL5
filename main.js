73b6328c7355f8b8a89c2ba766984c894c79173e
ArturoMedrano <arturohmedrano@gmail.com>
2015-02-10 16:16:12 -0500

    add html

---
+++ main.js
@@ -0,0 +1,31 @@
+//var shapes =
+//var colors =
+
+//var together = [shapes, colors];
+
+alert("Hello World");
+
+
+function addheadline() {
+   document.getElementById("H1").innerHTML = "Java Exercise Page";
+}
+
+var brand = ["porshe", "cadillac", "honda"];
+var type = ["SUV", "convertible", "sedan"]; 
+var carcombo = [brand, type];
+console.log(carcombo[0][1]);
+console.info(brand.length);
+
+var age = 16
+
+if (age<21){
+	console.log ("You can't purchase alcohol")
+} else {
+	console.log ("Drink responsibly")
+}
+
+for (var age = 16; age<21; age++){
+	console.log(age)
+}
+
+
